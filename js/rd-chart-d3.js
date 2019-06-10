ecfe0625be1f565110039ce231faf610fcf8ed3d
giulialcr <giulia.lucrezio@gmail.com>
2019-06-10 13:00:11 +0200

    first commit

---
+++ js/rd-chart-d3.js
@@ -0,0 +1,38 @@
+              /* Set-Up */
+              var color= d3.schemeCategory20b;
+              
+            var margin = {top: 120, right: 120, bottom: 120, left: 120},
+                width = Math.min(900, window.innerWidth - 10) - margin.left - margin.right,
+                height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
+            
+                
+            
+
+            /* Data */
+            
+            d3.json("data/dataset.json", function(data) {
+             
+            var data1=[]
+            for(i=0; i<data.length; i++){
+                data1[i]=d3.entries(data[i]);
+            }
+            console.log(data1);
+            console.log("data1");
+           
+            /* Draw the Chart */
+            
+            var color= d3.schemeCategory20b;
+                            
+            var radarChartOptions = {
+              w: width,
+              h: height,
+              margin: margin,
+              maxValue: 1, 
+              levels: 5,
+              roundStrokes: true, //true se li voglio arrotondati
+              color: color
+            };
+            /*Call function to draw the Radar chart */
+            RadarChart(".radarChart", data1, radarChartOptions);
+        });
+        
